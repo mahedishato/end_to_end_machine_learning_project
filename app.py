e2e848d98bf773c8f95194559060f1c86594869d
Mahedi Hasan Rasel <91797593+mahedishato@users.noreply.github.com>
2024-02-25 16:28:38 +0600

    Update app.py

--- app.py
+++ app.py
@@ -30,4 +30,4 @@ def predict(request: Request, CRIM: float = Form(...), ZN: float = Form(...), IN
 if __name__ == "__main__":
     import uvicorn
 
-    uvicorn.run(app, host="127.0.0.1", port=8000)
+    uvicorn.run(app, host="0.0.0.0", port=7860)
